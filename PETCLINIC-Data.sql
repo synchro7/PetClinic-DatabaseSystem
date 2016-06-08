fbdaacadc5eba1b0aafa5b8b71206ee808b32c71
ohmahgawd808 <nnt594@me.com>
2016-06-08 11:54:35 +0700

    Added Scar & Filet Minyon

--- PETCLINIC-Data.sql
+++ PETCLINIC-Data.sql
@@ -1,8 +1,13 @@
 USE PETCLINIC;
 
 INSERT INTO OWNER
-VALUES ('100001', 'Allen', 'Walker', 'GB', '001-000-111','M');
+VALUES 
+('100001', 'Allen', 'Walker', 'GB', '001-000-111','M'),
+('100002', 'Filet', 'Minyon', 'US', '001-000-112','F');
 
 INSERT INTO PET
-VALUES ('2000001', '100001', 'Puck', null, 'Dog','F');
+VALUES 
+('2000001', '100001', 'Puck', null, 'Faerie Dragon','F'),
+('2000002', '100002', 'Scar', 1994-06-15, 'Lion','M');
+
 
