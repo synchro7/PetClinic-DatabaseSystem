b92ae910ce3e6d2431c39afe381a4c8560d70f77
Latte <tae_synchronize@hotmail.com>
2016-06-12 01:01:41 +0700

    Add more data

--- PETCLINIC-Data.sql
+++ PETCLINIC-Data.sql
@@ -1,13 +1,21 @@
 USE PETCLINIC;
 
 INSERT INTO OWNER
-VALUES 
+VALUES
 ('100001', 'Allen', 'Walker', 'GB', '001-000-111','M'),
-('100002', 'Filet', 'Minyon', 'US', '001-000-112','F');
+('100002', 'Filet', 'Minyon', 'US', '001-000-112','F'),
+('110000', 'Alexander', 'Harper', 'GB', '109-000-999','M'),
+('110002', 'Pewdiepie', 'Kjellberg', 'SE', '123-000-933','M'),
+('110003', 'Phakphum', 'Wanpen', 'TH', '000-000-000','M');
 
 INSERT INTO PET
-VALUES 
+VALUES
 ('2000001', '100001', 'Puck', '1994-06-15', 'Faerie Dragon','F'),
-('2000002', '100002', 'Scar', '1994-06-15', 'Lion','M');
+('2000002', '100002', 'Scar', '1994-06-15', 'Lion','M'),
+('2100000', '110000', 'Pug', '1994-06-15', 'Faerie Dragon','F'),
+('2100002', '110002', 'Edgar', '2012-05-16', 'Pug','M'),
+('2100003', '110003', 'BaBlu', '2012-05-16', 'Pitbull','M');
+
+
 
 
