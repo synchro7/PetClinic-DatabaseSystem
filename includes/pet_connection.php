9e0fc74c6094428fc9768f74d3edc990bf1c8e31
Latte <tae_synchronize@hotmail.com>
2016-06-13 15:52:54 +0700

    One last time

--- includes/pet_connection.php
+++ includes/pet_connection.php
@@ -12,46 +12,7 @@
 	};
 
 	class PetConnection extends dbConn {
-		public function updateEmployee($imageLink, $id) {
-			include 'includes/config.php';
-			try {
-				$db = new dbConn;
-				$err = '';
-				// prepare sql and bind parameters
-				$stmt = $db->conn->prepare("UPDATE BANNER_INDEX SET image_link = :image_link WHERE id = :id");
-				$stmt->bindParam(':image_link', $imageLink);
-				$stmt->bindParam(':id', $id);
-				$stmt->execute();
-			}
-			catch (PDOException $e) {
-				$err = "Error: " . $e->getMessage();
-			}
-			//Determines returned value ('true' or error code)
-			if ($err == '') {
-				$success = 'true';
-			}
-			else {
-				$success = $err;
-			};
 
-			return $success;
-		}
-		public function deleteEmployee($image_id) {
-			include 'includes/config.php';
-			try {
-				$db = new dbConn;
-				$err = '';
-				$stmt = $db->conn->prepare("DELETE FROM BANNER_INDEX WHERE id = :image_id");
-				$stmt->bindParam(':image_id', $image_id);
-				$stmt->execute();
-			}
-				catch (PDOException $e) {
-				$err = "Error: " . $e->getMessage();
-			}
-			//Determines returned value ('true' or error code)
-			$success = ($err == '') ? 'true' : $error;
-			return $success;
-		}
 		public function getPets() {
 			include 'includes/config.php';
 			try {
@@ -105,31 +66,6 @@
 				$err = "Error: " . $e->getMessage();
 			}
 			$success = ($err == '' ? $results : $err );
-
-			return $success;
-		}
-		public function createEmployee($imagePath, $imageUserPath, $imageLink) {
-			include 'includes/config.php';
-			try {
-				$db = new dbConn;
-				$err = '';
-				// prepare sql and bind parameters
-				$stmt = $db->conn->prepare("INSERT INTO BANNER_INDEX(admin_path, image_path, image_link, status) VALUES (:image_path, :user_path, :image_link, 'active')");
-				$stmt->bindParam(':image_path', $imagePath);
-				$stmt->bindParam(':user_path', $imageUserPath);
-				$stmt->bindParam(':image_link', $imageLink);
-				$stmt->execute();
-			}
-			catch (PDOException $e) {
-				$err = "Error: " . $e->getMessage();
-			}
-			//Determines returned value ('true' or error code)
-			if ($err == '') {
-				$success = 'true';
-			}
-			else {
-				$success = $err;
-			};
 
 			return $success;
 		}
--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php include ('includes/header.php'); ?>
-<?php include ("includes/pet_connection.php"); ?>
 <center><img src="images/logo.png" class="img-responsive"></center>
 <?php include ('includes/footer.php'); ?>
